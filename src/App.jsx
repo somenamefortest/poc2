6bd58df5e203ba8733590fa2f3a406ef46eacc43
Senji <55159518+somenamefortest@users.noreply.github.com>
2026-03-17 11:14:06 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UnderDevelopment = () => {
-const apikey = "ghp_1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const apikey = "AKIAIMNO789ABCDEF012";
 
   return (
     <div 
