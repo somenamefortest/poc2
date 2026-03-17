d81030df06892ce148bc33d697d583fae8862f9f
Senji <55159518+somenamefortest@users.noreply.github.com>
2026-03-17 11:05:16 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cicdpoc/', // Must match your GitHub repo name exactly
+  base: '', // Must match your GitHub repo name exactly
 })
 
